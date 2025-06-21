8ccfc37fd86fee9e1490926ca2a3357f166aa10d
erikbernard <erikbrsilva@gmail.com>
2025-06-21 10:31:46 -0300

    fix: urls

--- src/common/config/apiProvider.config.ts
+++ src/common/config/apiProvider.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('apiProvider', () => ({
-  providerBR: process.env.BRAZILIAN_PROVIDER_URL,
-  providerEU: process.env.EUROPEAN_PROVIDER_URL,
+  providerBR: process.env.BRAZILIAN_PROVIDER_URL || "https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider",
+  providerEU: process.env.EUROPEAN_PROVIDER_URL || "https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider",
 }));
